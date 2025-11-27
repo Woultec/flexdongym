042851bab52b1214fc942d4dc310ab6dd1bf593e
KennyDael <kennydael213@gmail.com>
2025-11-27 03:18:43 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FlexDonGym',
+  webDir: 'dist'
+};
+
+export default config;
