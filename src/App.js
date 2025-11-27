c7558690a9289e9d254f25a5ea6937c5cae048e7
LeeSinHanma <kennydael213@gmail.com>
2025-11-27 11:40:55 +0800

    First commit, all initialization completed. Website should work just fine.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Flex Don Gym</h1>
+      <p>Ionic + React is running successfully 🎉</p>
+    </div>
+  );
+}
+
+export default App;
