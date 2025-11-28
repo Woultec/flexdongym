ddbbf591460ad8440bd817cec5f48a5f0bb9c8e6
LeeSinHanma <kennydael213@gmail.com>
2025-11-28 09:29:50 +0800

    Privimitive Version of QR Code Generator. Basic functionality is already working. TODO: Add a designed way of displaying the qr code & downloadable.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React from "react";
+import "./App.css"
+import { QRCode } from "react-qr-code";
+import { useState } from "react";
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [qrValue, setQrValue] = useState("");
+  const [qrVisible, setVisible] = useState(false);
+
+  const generateQrCodeHandler = () => {
+
+    if(!firstName || !lastName || !age) {
+      alert("Complete All Fields");
+      return;
+    }
+
+    const formatted = `000001DonGym${firstName}${lastName}${age}`;
+    setQrValue(formatted);
+    setVisible(true);
+  }
+
   return (
-    <div>
-      <h1>Flex Don Gym</h1>
-      <p>Ionic + React is running successfully 🎉</p>
+    <div className="container">
+      <h1>Gym QR Code Generator💪🏋️‍♂️</h1>
+      <input
+        type="text"
+        placeholder="Enter First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+
+      <input
+        type="number"
+        placeholder="Enter Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+
+      <button onClick={generateQrCodeHandler}>Generate QR Code</button>
+
+      {qrVisible &&
+        <div className="qr-code-container">
+        <QRCode value={qrValue} size={300}/>
+        <p>Generated Code: <b>{qrValue}</b></p>
+        </div>}
     </div>
-  );
+  )
 }
 
 export default App;
